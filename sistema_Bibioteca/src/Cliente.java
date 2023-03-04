35d59d84e93514e403ebdbf9fc31afed31829409
Ygor Kwan <igorkwan2022@gmail.com>
2023-03-04 13:43:05 -0300

    Atualização com Base de dados de Cliente Feita

--- sistema_Bibioteca/src/Cliente.java
+++ sistema_Bibioteca/src/Cliente.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
+
 public class Cliente {
     private String nome;
+    private static String cpf;
     private int idade;
-    private String endereco;
+    private String rua;
+    private String cidade;
 
-    public Cliente(String nome, int idade, String endereco) {
+    public Cliente(String nome,String cpf, int idade, String rua, String cidade) {
         this.nome = nome;
+        this.cpf = cpf;
         this.idade = idade;
-        this.endereco = endereco;
+        this.rua = rua;
+        this.cidade = cidade;
     }
 
     public String getNome() {
@@ -25,20 +31,70 @@ public class Cliente {
         this.idade = idade;
     }
 
-    public String getEndereco() {
-        return endereco;
+    public String getRua() {
+        return rua;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     @Override
     public String toString() {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
                 ", idade=" + idade +
-                ", endereco='" + endereco + '\'' +
+                ", rua='" + rua + '\'' +
+                ", cidade='" + cidade + '\'' +
                 '}';
+    }
+
+    public static void criarCliente(String nome, String cpf, int idade, String rua, String cidade, ArrayList<Cliente> clientes) {
+        Cliente novoCliente = new Cliente(nome, cpf, idade, rua, cidade);
+        clientes.add(novoCliente);
+    }
+
+    public static void atualizarCliente(String nome, String novoNome ,int novaIdade, String novaRua, String novaCidade, ArrayList<Cliente> clientes) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getNome() == nome) {
+                cliente.setNome(novoNome);
+                cliente.setIdade(novaIdade);
+                cliente.setRua(novaRua);
+                cliente.setCidade(novaCidade);
+                break;
+            }
+        }
+    }
+    public static void deletarCliente(String nome, ArrayList<Cliente> clientes) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getNome() == nome) {
+                clientes.remove(cliente);
+                break;
+            }
+        }
+    }
+
+
+    public static void listarClientes(ArrayList<Cliente> clientes) {
+        for (Cliente cliente : clientes) {
+            System.out.println("Cpf: " + cliente.getCpf() + ", Nome: " + cliente.getNome() + ", Idade: " + cliente.getIdade() + ", Rua: " + cliente.getRua() + ", Cidade: " + cliente.getCidade());
+        }
     }
 }
--- sistema_Bibioteca/src/Main.java
+++ sistema_Bibioteca/src/Main.java
@@ -3,21 +3,34 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Livro> livrosBiblioteca = new ArrayList<>();
+        ArrayList<Livro> livrosBiblioteca = new ArrayList<>();
+        ArrayList<Cliente> clientesBilioteca = new ArrayList<>();
+
+        System.out.println("---------------------------------Sistema de Biblioteca---------------------------------");
+
+        // criar clientes biblioteca
+        Cliente.criarCliente("João Filho","1456321",18,"Rua dos Alfeneiros","São José da Lagoa Tapada", clientesBilioteca);
+        Cliente.criarCliente("Marcos Paulo","558145",21,"Rua Marcos Valadares","São José do Rio Preto", clientesBilioteca);
+        Cliente.criarCliente("Elias Monkbiel","478963",35,"Rua José de Sa","São Paulo", clientesBilioteca);
+       Cliente.criarCliente("Manoel Gomes","234569",50,"Rua Duda Xerques","Rio de Janeiro", clientesBilioteca);
+        //atualizar cliente
+       //Cliente.atualizarCliente("João Filho","Ze de Nina",20,"Rua Geraldo de Sá","Lagoa Bom Jesus", clientesBilioteca);
+        //deletar cliente
+        Cliente.deletarCliente("Elias Monkbiel", clientesBilioteca);
+        //listar Clientes
+       Cliente.listarClientes(clientesBilioteca);
+
 
         // criar livro
-        Livro.criarLivro("livro 1", "Douglas Adams", 193, (ArrayList<Livro>) livrosBiblioteca);
-        Livro.criarLivro("livro 2", "Douglas Adams", 193, (ArrayList<Livro>) livrosBiblioteca);
-        Livro.criarLivro("livro 3", "Douglas Adams", 193, (ArrayList<Livro>) livrosBiblioteca);
-
+        Livro.criarLivro("livro 1", "Douglas Adams", 193, livrosBiblioteca);
+       Livro.criarLivro("livro 2", "Douglas Adams", 193, livrosBiblioteca);
+      Livro.criarLivro("livro 3", "Douglas Adams", 193, livrosBiblioteca);
         // atualizar livro
-        Livro.atualizarLivro("livro 1","O Restaurante no Fim do Universo", "Ygor", 45, (ArrayList<Livro>) livrosBiblioteca);
-
+      //  Livro.atualizarLivro("livro 1","O Restaurante no Fim do Universo", "Ygor", 45, livrosBiblioteca);
         // deletar livro
-        Livro.deletarLivro("livro 2", (ArrayList<Livro>) livrosBiblioteca);
-
+      //  Livro.deletarLivro("livro 2", livrosBiblioteca);
         // listar todos os livros
-        Livro.listarLivros((ArrayList<Livro>) livrosBiblioteca);
+       Livro.listarLivros(livrosBiblioteca);
 
 
     }
